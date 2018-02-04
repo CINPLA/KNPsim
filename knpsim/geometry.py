1dda8256c500b593f7ed475b7879c6bcba1a9b4b
Andreas V. Solbrå <a.v.solbra@fys.uio.no>
2018-02-04 17:55:34 +0100

    docstrings Geometry

--- knpsim/geometry.py
+++ knpsim/geometry.py
@@ -1,20 +1,28 @@
 from dolfin import *
 
 
-# TODO: Add an explenation of the different types the mesh can be
 class Geometry:
     """
     This class keeps track of mesh and the function space of the electro-
     diffusion solver. It should be the first class initialized by the user.
 
-    G
+    Args:
+        mesh (Mesh, list, or str): The mesh to simulate on. Can be a FEniCS
+            Mesh instance, a string to a file containing a mesh, or a list of
+            numbers specifying the number of elements in each spatial
+            direction.
+        element (str, optional): A string spesifying the element to be used.
+            Passed to the FEniCS constructor FiniteElement().
+        order (int, optional): The order of the element. Passed to
+            FiniteElement()
     """
-    def __init__(self, mesh, space='CG', order=1):
+    def __init__(self, mesh, element='P', order=1):
         self.meshtype = mesh
         self.space = space
         self.order = order
         self.element_type_list = [interval, triangle, tetrahedron]
 
+        # Set mesh
         if isinstance(mesh, list):
             domain_type = [UnitIntervalMesh, UnitSquareMesh, UnitCubeMesh]
             self.dim = len(mesh)
@@ -44,7 +52,9 @@ class Geometry:
             print("input not understood! Exiting...")
             sys.exit(1)
 
-        self.P1 = FiniteElement('P', self.element_type_list[self.dim-1], 1)
+        # Set elements:
+        self.P1 = FiniteElement(element, self.element_type_list[self.dim-1],
+                                order)
         self.R0 = FiniteElement('R', self.element_type_list[self.dim-1], 0)
-        self.V = FunctionSpace(mesh, space, order)
-        self.R = FunctionSpace(mesh, "R", 0)
+        self.V = FunctionSpace(mesh, self.P1)
+        self.R = FunctionSpace(mesh, "R", self.R0)
