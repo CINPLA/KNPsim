6f02fe8771f1c3cee03b478c26233778b18e3851
Andreas Solbrå <andreas_vso@hotmail.com>
2018-03-02 19:02:16 +0100

    added app 3 shell script

---
+++ examples/hay_model/run_application_3_fast.sh
@@ -0,0 +1,9 @@
+file="neuron_input_1.h5"
+if [ ! -f "$file" ]
+then
+    echo "$0: File '${file}' not found. Fetching from figshare.";
+    wget https://ndownloader.figshare.com/files/10622146
+    mv 10622146 neuron_input_1.rar
+    unrar e neuron_input_1.rar
+fi
+python run_3d_column.py
